0cd1c52a42d37201be91c921dc428292b1f13a2b
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-21 17:35:27 +0300

    edited main

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -11,9 +11,21 @@ public class QuadraticEquation {
         double a = scanner.nextDouble();
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
+        double discriminant;
+        discriminant = (b*b) - (4 * a * c);
 
-
-
+        if(discriminant > 0){
+            double result1 =(((0 - b) - Math.sqrt(discriminant)) / (2 * a));
+            double result2 = ((0 - b) + Math.sqrt(discriminant)) / (2 * a);
+            System.out.println(result1 + " " + result2);
+        }
+        else if(discriminant == 0){
+            double result1 = (0 - b) / (2 * a);
+            System.out.println(result1);
+        }
+        else{
+            System.out.println("no roots");
+        }
     }
 
 }
